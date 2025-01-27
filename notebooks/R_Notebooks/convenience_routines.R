eae0307be4b978e9e9b1d28f24cc9ef05b1de405
tstuckey <tstuckey@simplifyingcomplexspaces.com>
2025-01-26 19:35:16 -0700

    typechange

--- notebooks/R_Notebooks/convenience_routines.R
+++ notebooks/R_Notebooks/convenience_routines.R
@@ -1,1 +1,14 @@
-../../../../analysis environment/structure/notebooks/R_Notebooks/convenience_routines.R
+#' Get the database file
+#' 
+#' @param t_url string of the URL with the binary database file
+getRemoteDBfile <- function(t_url, t_local_file){
+    db_file <- download.file(t_url, t_local_file, mode = 'wb') 
+    return(db_file)
+}
+
+#` Get the MD5 hash 
+#`
+computeMD5hash <- function(t_path){
+    t_result <- tools::md5sum(t_path)
+    print(t_result)
+}
